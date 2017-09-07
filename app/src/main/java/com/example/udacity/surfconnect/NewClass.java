71be968d4bc7272369b5b643e453c471d172cbe2
Vitalii <morozvit.post@gmail.com>
2017-09-07 20:05:00 +0300

    some comment

--- app/src/main/java/com/example/udacity/surfconnect/NewClass.java
+++ app/src/main/java/com/example/udacity/surfconnect/NewClass.java
@@ -5,4 +5,28 @@ package com.example.udacity.surfconnect;
  */
 
 public class NewClass {
+    public int number;
+    public int numberOne;
+
+    public NewClass(int number, int numberOne) {
+        this.number = number;
+        this.numberOne = numberOne;
+    }
+
+    public int getNumber() {
+        //git
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumberOne() {
+        return numberOne;
+    }
+
+    public void setNumberOne(int numberOne) {
+        this.numberOne = numberOne;
+    }
 }
